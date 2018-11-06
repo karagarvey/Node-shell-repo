ed796ea090fff0ba6a7ab160a6cb923157df0721
Norka Avalos <norka.avalos11@gmail.com>
2018-11-06 15:29:46 -0500

    almost done with ls

--- bash.js
+++ bash.js
@@ -1,15 +1,4 @@
 process.stdout.write('prompt > ');
 
-process.stdin.on('data', function (data) {
-  const cmd = data.toString().trim();
-
-
-
-  if (cmd === 'pwd'){
-    console.log(__dirname);
-    process.stdout.write('prompt > ');
-  } else {
-    process.stdout.write('You typed ' + cmd);
-    process.stdout.write('prompt > ');
-  }
-})
+const exportsFromPwd = require('./pwd.js');
+exportsFromPwd();
